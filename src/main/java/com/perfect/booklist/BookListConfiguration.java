2569e7ad66e95c2cf2b735fbef6375c6f9fdf8c4
Zloy <kgirko@nas.company>
2017-08-22 23:40:17 +0500

    init

---
+++ src/main/java/com/perfect/booklist/BookListConfiguration.java
@@ -0,0 +1,51 @@
+package com.perfect.booklist;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Created by Zloy on 16.08.2017.
+ */
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.perfect.booklist")
+public class BookListConfiguration extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        registry.viewResolver(viewResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/include/**").addResourceLocations("/include/");
+        registry.addResourceHandler("/app/**").addResourceLocations("/resources/app/");
+        registry.addResourceHandler("/build/**").addResourceLocations("/resources/build/");
+        registry.addResourceHandler("/ext/**").addResourceLocations("/resources/ext/");
+        registry.addResourceHandler("/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("/classic/**").addResourceLocations("/resources/classic/");
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("messages");
+        return messageSource;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer matcher) {
+        matcher.setUseRegisteredSuffixPatternMatch(true);
+    }
+}
---
+++ src/main/java/com/perfect/booklist/BookListInitial.java
@@ -0,0 +1,30 @@
+package com.perfect.booklist;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Created by Zloy on 16.08.2017.
+ */
+public class BookListInitial extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[] { BookListConfiguration.class };
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        return new Filter []  {  };
+    }
+}
